5f44cbe91557b66a95abc300ded4ef3045bf886d
Clinton Yade <yadeclinton20@gmail.com>
2025-10-17 11:40:48 +0300

    added the tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF", // deep premium blue
+        secondary: "#FACC15", // elegant yellow accent
+      },
+    },
+  },
+  plugins: [],
+};
