0546aa9a5534d0cd8f9b84753f718fd29b8b67ba
Clinton Yade <yadeclinton20@gmail.com>
2025-10-20 00:25:58 +0300

    updated hooks

--- app/hooks/use-cart.ts
+++ app/hooks/use-cart.ts
@@ -1,39 +1,44 @@
 import { create } from "zustand";
 
-interface Product {
+interface CartItem {
   id: string;
   name: string;
-  description: string;
-  image: string;
   price: number;
-  isFeatured?: boolean;
+  image: string;
+  quantity: number;
 }
 
 interface CartStore {
-  items: Product[];
-  addToCart: (product: Product) => void;
-  removeFromCart: (id: string) => void;
-  buyNow: (product: Product) => void;
-  total: number;
+  items: CartItem[];
+  addItem: (item: Omit<CartItem, "quantity">) => void;
+  removeItem: (id: string) => void;
+  clearCart: () => void;
+  cartCount: number;
 }
 
 export const useCart = create<CartStore>((set, get) => ({
   items: [],
-  addToCart: (product) =>
-    set((state) => ({
-      items: [...state.items, product],
-    })),
+  addItem: (item) => {
+    const existingItem = get().items.find((i) => i.id === item.id);
 
-  removeFromCart: (id: string) =>
-    set((state) => ({
-      items: state.items.filter((item) => item.id !== id),
-    })),
-
-  buyNow: (product) => {
-    console.log("Buy now clicked:", product);
+    if (existingItem) {
+      set({
+        items: get().items.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+        ),
+      });
+    } else {
+      set({ items: [...get().items, { ...item, quantity: 1 }] });
+    }
   },
 
-  total: 0,
-}));
+  removeItem: (id) => {
+    set({ items: get().items.filter((i) => i.id !== id) });
+  },
 
-// We can add a computed selector in the cart page
+  clearCart: () => set({ items: [] }),
+
+  get cartCount() {
+    return get().items.reduce((total, item) => total + item.quantity, 0);
+  },
+}));
